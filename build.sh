260b0ba8e950e667ac866ea1b5f7c210dc699cea
pauline12ish34 <paulineishimwe12@gmail.com>
2025-07-28 13:49:05 +0200

    rm attributes

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Install Git LFS and fetch large files (like .pkl)
-git lfs install
-git lfs pull
-
 # Optional: install your dependencies
 pip install -r requirements.txt
