0308da17fa57b60fb33ec50a1eeb2464eaf8700e
pauline12ish34 <paulineishimwe12@gmail.com>
2025-07-28 17:44:21 +0200

    download model in build

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 # Optional: install your dependencies
+#!/usr/bin/env bash
+# build.sh - Render will run this at build time
+
+# Ensure directories
+mkdir -p models
+
+# Download the model
+echo "Downloading model..."
+curl -L -o models/best_model.pkl https://github.com/pauline12ish34/summative_linear_regression/releases/download/v1.0.0/best_model.pkl
+
+echo "Model download complete."
 pip install -r requirements.txt
